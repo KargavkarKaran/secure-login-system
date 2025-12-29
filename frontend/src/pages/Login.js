97291823b8ce4a5b2c8bc747f0fcf361acd979be
KargavkarKaran <karankargavkar@gmail.com>
2025-12-29 22:50:24 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -26,7 +26,7 @@ function Login() {
             return handleError(' email and password are required')
         }  
         try {
-            const url = "http://localhost:5000/auth/login";
+            const url = "https://secure-login-system-api.vercel.app/auth/login";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
